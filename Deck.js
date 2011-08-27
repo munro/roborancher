a70632215f2b4abd7b277abdbd6a3f80867ef126
Oliver Smith <osmith@gmail.com>
2011-08-26 18:28:22 -0700

    Very basic deck

---
+++ Deck.js
@@ -0,0 +1,29 @@
+function Deck() {
+  var util = require('./util');
+  this.queue = new util.PriorityQueue();
+};
+
+Deck.prototype.addCard = function(card) {
+  this.queue.push(Math.random(), card);
+}
+
+Deck.prototype.drawCard = function() { 
+  return this.queue.pop();
+}
+
+
+
+/*
+ * unit tests
+ */
+if(0 == 1) {
+  var deck = new Deck();
+  
+  deck.addCard('card1');
+  deck.addCard('card2');
+   
+  console.log(deck.drawCard());
+  console.log(deck.drawCard());
+  console.log(deck.drawCard());
+}
+
