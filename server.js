f269e5f6acecad99708e0609fa7c072127d3cc52
Andrew Alm <awalm@localhostess.localnet>
2011-08-26 20:21:00 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+  nko = require('nko')('BB3sRa2b2FOSfcCw');
+
+var app = http.createServer(function (req, res) { 
+  res.writeHead(200, { 'Content-Type': 'text/html' }); 
+  res.end('Hello, World'); 
+});
+
+app.listen(parseInt(process.env.PORT) || 7777); 
+console.log('Listening on ' + app.address().port);
+
